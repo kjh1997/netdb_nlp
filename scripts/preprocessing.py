b13c694709d8a108371c68c31fd5c85fbfeffe28
kjh1997 <aam41@naver.com>
2022-03-14 14:19:20 +0900

    w2v

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -3,12 +3,15 @@ sys.path.append('C:/Users/sudo/Desktop/git/netdb_nlp')
 sys.path.append('/home/kjh/netdb_nlp')
 import json, math, codecs, datetime
 from os.path import join
-
+from collections import defaultdict as dd
 from utils.cache import LMDBClient
-
+from global_.embedding import EmbeddingModel
 import pprint
 from utils import data_utils, settings, feature_utils
+from datetime import datetime
+from time import sleep
 
+start_time = datetime.now()
 pubs_base = 'pubs_raw.json'
 def dump_author_features_to_file():
     pubs_dict = data_utils.load_json(settings.GLOBAL_DATA_DIR, pubs_base )
@@ -40,7 +43,6 @@ def dump_author_features_to_cache():
                 print('line', i)
             items = line.rstrip().split('\t')
             pid_order = items[0]
-            print(items)
             author_features = items[1].split()
             lc.set(pid_order, author_features)
 def cal_feature_idf():
@@ -67,6 +69,34 @@ def cal_feature_idf():
         idf[k] = math.log(cnt / counter[k])
     data_utils.dump_data(dict(idf), feature_dir, "feature_idf.pkl")
 
+
+def dump_author_embs():
+    """
+    dump author embedding to lmdb
+    author embedding is calculated by weighted-average of word vectors with IDF
+    """
+    emb_model = EmbeddingModel.Instance()
+    idf = data_utils.load_data(settings.GLOBAL_DATA_DIR, 'feature_idf.pkl')
+    print('idf loaded')
+    LMDB_NAME_FEATURE = 'pub_authors.feature'
+    lc_feature = LMDBClient(LMDB_NAME_FEATURE)
+    LMDB_NAME_EMB = "author_100.emb.weighted"
+    lc_emb = LMDBClient(LMDB_NAME_EMB)
+    cnt = 0
+    with lc_feature.db.begin() as txn:
+        for k in txn.cursor():
+            if cnt % 1000 == 0:
+                print('cnt', cnt, datetime.now()-start_time)
+            cnt += 1
+            pid_order = k[0].decode('utf-8')
+            features = data_utils.deserialize_embedding(k[1])
+            cur_emb = emb_model.project_embedding(features, idf)
+            if cur_emb is not None:
+                lc_emb.set(pid_order, cur_emb)
+
+
+
+
 if __name__ == '__main__':
     """
     some pre-processing
@@ -76,7 +106,7 @@ if __name__ == '__main__':
     emb_model = EmbeddingModel.Instance()
     emb_model.train('aminer')  # training word embedding model
     cal_feature_idf()
-#    dump_author_embs()
+    dump_author_embs()
     print('done', datetime.now()-start_time)
 
 
