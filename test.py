130ba0e7df240b94a9ec8109f9a8d01f277a139a
kjh <aam41@naver.com>
2022-03-14 15:11:00 +0900

    test

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
-a = [1,2,3]
+class test:
+    def __init__(self, decorated):
+        self.data = "123"
+        self._decorated = decorated
 
-print(a.index(3))
+    def test123(self):
+        print(self.data)
+
+    def __call__(self):
+        print(self._decorated.__name__)
+        self._decorated()
+        print(self._decorated.__name__)
+            
+
+@test
+def test12():
+    print("함수호출")
+
+test12()
+
+
